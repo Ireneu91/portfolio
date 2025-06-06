36b2a7d2abf1c5094c223b84feb204fc8001dc0d
Ireneu91 <irene.ucciero91@gmail.com>
2025-06-06 11:59:25 +0200

    Struttura fascia azzurra

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 import { Cover } from "@/components/Cover";
 import {Projects} from "@/components/Projects"
 
+
+
 // import '@/assets/fonts/'
 import '@/assets/global.css'
 
 export default function Home() {
   return (
     <main className="container mx-auto">
-      <h1 className="bg-red-900 text-xl">Il mio sito</h1>
-      <div>
-        <span>aaa</span>
-        <p>asd</p>
-      </div>
       <Cover />
       <Projects />
     </main>
--- src/components/Cover.tsx
+++ src/components/Cover.tsx
@@ -2,12 +2,83 @@
 
 export function Cover() {
     return (
-        <div className="grid grid-cols-2 sm:grid-cols-3 gap-6">
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
-            <div>5</div>
-        </div>
+        <section id="fascia_azzurra" className="grid grid-cols-3 xs:grid-cols-1">
+            <div className="foto_irene"></div>
+            <div className="colonna_destra col-span-2">
+                <h1>Irene Ucciero</h1>
+                <h2>Portfolio</h2>
+                <p className="bg-black titoletto">Ho lavorato con:</p>
+                <div className="loghetti flex-wrap">
+                    <div class="inline-flex">
+                        <img width="" height="" alt="Adobe Illustrator" title="Adobe Illustrator" src={"@/images/loghetto-illustrator.png"} />
+                        <p>Adobe Illustrator</p>
+                    </div>
+                    <div class="inline-flex">
+                        <img width="" height="" alt="Adobe Photoshop" title="Adobe Photoshop" src={"@/images/loghetto-photoshop.png"} />
+                        <p>Adobe Photoshop</p>
+                    </div>
+                    <div class="inline-flex">
+                        <img width="" height="" alt="Html" title="Html" src={"@/images/loghetto-html.svg"} />
+                        <p>Html</p>
+                    </div>
+                    <div class="inline-flex">
+                        <img width="" height="" alt="Css" title="Css" src={"@/images/loghetto-css.svg"} />
+                        <p>Css</p>
+                    </div>
+                    <div class="inline-flex">
+                        <img width="" height="" alt="Javascript" title="Javascript" src={"@/images/loghetto-js.svg"} />
+                        <p>Javascript</p>
+                    </div>
+                    <div class="inline-flex">
+                        <img width="" height="" alt="Atom" title="Atom" src={"@/images/loghetto-atom.svg"} />
+                        <p>Atom</p>
+                    </div>
+                    <div class="inline-flex">
+                        <img width="" height="" alt="Php" title="Php" src={"@/images/loghetto-php.svg"} />
+                        <p>Php</p>
+                    </div>
+                    <div class="inline-flex">
+                        <img width="" height="" alt="Vs Code" title="Vs Code" src={"@/images/loghetto-vscode.svg"} />
+                        <p>Vs Code</p>
+                    </div>
+                    <div class="inline-flex">
+                        <img width="" height="" alt="Affinity Design" title="Affinity Design" src={"@/images/loghetto-affinity.svg"} />
+                        <p>Affinity Design</p>
+                    </div>
+                    <div class="inline-flex">
+                        <img width="" height="" alt="Filmora" title="Filmora" src={"@/images/loghetto-filmora.png"} />
+                        <p>Filmora</p>
+                    </div>
+                    <div class="inline-flex">
+                        <img width="" height="" alt="Chat Gpt" title="Chat Gpt" src={"@/images/loghetto-chatgpt.svg"} />
+                        <p>Chat Gpt</p>
+                    </div>
+                    <div class="inline-flex">
+                        <img width="" height="" alt="React" title="React" src={"@/images/"} />
+                        <p>React</p>
+                    </div>
+                    <div class="inline-flex">
+                        <img width="" height="" alt="Next.Js" title="Next.Js" src={"@/images/"} />
+                        <p>Next.Js</p>
+                    </div>
+                    <div class="inline-flex">
+                        <img width="" height="" alt="Git" title="Git" src={"@/images/"} />
+                        <p>Git</p>
+                    </div>
+                    <div class="inline-flex">
+                        <img width="" height="" alt="GitHub" title="GitHub" src={"@/images/"} />
+                        <p>GitHub</p>
+                    </div>
+                    <div class="inline-flex">
+                        <img width="" height="" alt="Tailwindcss" title="Tailwindcss" src={"@/images/"} />
+                        <p>TailwindCss</p>
+                    </div>
+                    <div class="inline-flex">
+                        <img width="" height="" alt="Bootstrap" title="Bootstrap" src={"@/images/"} />
+                        <p>Bootstrap</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
