f72182bf8636a53fa2247c4ad374bd1df242c952
Ireneu91 <irene.ucciero91@gmail.com>
2025-06-15 13:32:52 +0200

    <aggiunte le immagini>

--- src/components/Cover.tsx
+++ src/components/Cover.tsx
@@ -11,72 +11,72 @@ export function Cover() {
                 <h2>Portfolio</h2>
                 <p className="bg-black titoletto">Ho lavorato con:</p>
                 <div className="loghetti flex-wrap">
-                    <div className="inline-flex">
-                        <Image width="50" height="50" alt="Adobe Illustrator" title="Adobe Illustrator" src={"@/images/loghetto-illustrator.png"} />
+                    <div className="loghetti inline-block">
+                        <Image width="50" height="50" alt="Adobe Illustrator" title="Adobe Illustrator" src={"/images/loghetto-illustrator.png"} />
                         <p>Adobe Illustrator</p>
                     </div>
-                    <div className="inline-flex">
-                        <Image width="50" height="50" alt="Adobe Photoshop" title="Adobe Photoshop" src={"@/images/loghetto-photoshop.png"} />
+                    <div className="loghetti inline-block">
+                        <Image width="50" height="50" alt="Adobe Photoshop" title="Adobe Photoshop" src={"/images/loghetto-photoshop.png"} />
                         <p>Adobe Photoshop</p>
                     </div>
-                    <div className="inline-flex">
-                        <Image width="50" height="50" alt="Html" title="Html" src={"@/images/loghetto-html.svg"} />
+                    <div className="loghetti inline-block">
+                        <Image width="50" height="50" alt="Html" title="Html" src={"/images/loghetto-html.svg"} />
                         <p>Html</p>
                     </div>
-                    <div className="inline-flex">
-                        <Image width="50" height="50" alt="Css" title="Css" src={"@/images/loghetto-css.svg"} />
+                    <div className="loghetti inline-block">
+                        <Image width="50" height="50" alt="Css" title="Css" src={"/images/loghetto-css.svg"} />
                         <p>Css</p>
                     </div>
-                    <div className="inline-flex">
-                        <Image width="50" height="50" alt="Javascript" title="Javascript" src={"@/images/loghetto-js.svg"} />
+                    <div className="loghetti inline-block">
+                        <Image width="50" height="50" alt="Javascript" title="Javascript" src={"/images/loghetto-js.svg"} />
                         <p>Javascript</p>
                     </div>
-                    <div className="inline-flex">
-                        <Image width="50" height="50" alt="Atom" title="Atom" src={"@/images/loghetto-atom.svg"} />
-                        <p>Atom</p>
-                    </div>
-                    <div className="inline-flex">
-                        <Image width="50" height="50" alt="Php" title="Php" src={"@/images/loghetto-php.svg"} />
+                    <div className="loghetti inline-block">
+                        <Image width="50" height="50" alt="Php" title="Php" src={"/images/loghetto-php.svg"} />
                         <p>Php</p>
                     </div>
-                    <div className="inline-flex">
-                        <Image width="50" height="50" alt="Vs Code" title="Vs Code" src={"@/images/loghetto-vscode.svg"} />
+                    <div className="loghetti inline-block">
+                        <Image width="50" height="50" alt="Atom" title="Atom" src={"/images/loghetto-atom.svg"} />
+                        <p>Atom</p>
+                    </div>
+                    <div className="loghetti inline-block">
+                        <Image width="50" height="50" alt="Vs Code" title="Vs Code" src={"/images/loghetto-vscode.svg"} />
                         <p>Vs Code</p>
                     </div>
-                    <div className="inline-flex">
-                        <Image width="50" height="50" alt="Affinity Design" title="Affinity Design" src={"@/images/loghetto-affinity.svg"} />
+                    <div className="loghetti inline-block">
+                        <Image width="50" height="50" alt="Affinity Design" title="Affinity Design" src={"/images/loghetto-affinity.svg"} />
                         <p>Affinity Design</p>
                     </div>
-                    <div className="inline-flex">
-                        <Image width="50" height="50" alt="Filmora" title="Filmora" src={"@/images/loghetto-filmora.png"} />
+                    <div className="loghetti inline-block">
+                        <Image width="50" height="50" alt="Filmora" title="Filmora" src={"/images/loghetto-filmora.png"} />
                         <p>Filmora</p>
                     </div>
-                    <div className="inline-flex">
-                        <Image width="50" height="50" alt="Chat Gpt" title="Chat Gpt" src={"@/images/loghetto-chatgpt.svg"} />
+                    <div className="loghetti inline-block">
+                        <Image width="50" height="50" alt="Chat Gpt" title="Chat Gpt" src={"/images/loghetto-chatgpt.svg"} />
                         <p>Chat Gpt</p>
                     </div>
-                    <div className="inline-flex">
-                        <Image width="50" height="50" alt="React" title="React" src={"@/images/"} />
+                    <div className="loghetti inline-block">
+                        <Image width="50" height="50" alt="React" title="React" src={"/images/loghetto-react.svg"} />
                         <p>React</p>
                     </div>
-                    <div className="inline-flex">
-                        <Image width="50" height="50" alt="Next.Js" title="Next.Js" src={"@/images/"} />
+                    <div className="loghetti inline-block">
+                        <Image width="50" height="50" alt="Next.Js" title="Next.Js" src={"/images/loghetto-nextjs.svg"} />
                         <p>Next.Js</p>
                     </div>
-                    <div className="inline-flex">
-                        <Image width="50" height="50" alt="Git" title="Git" src={"@/images/"} />
+                    <div className="loghetti inline-block">
+                        <Image width="50" height="50" alt="Git" title="Git" src={"/images/loghetto-git.svg"} />
                         <p>Git</p>
                     </div>
-                    <div className="inline-flex">
-                        <Image width="50" height="50" alt="GitHub" title="GitHub" src={"@/images/"} />
+                    <div className="loghetti inline-block">
+                        <Image width="50" height="50" alt="GitHub" title="GitHub" src={"/images/loghetto-github.svg"} />
                         <p>GitHub</p>
                     </div>
-                    <div className="inline-flex">
-                        <Image width="50" height="50" alt="Tailwindcss" title="Tailwindcss" src={"@/images/"} />
+                    <div className="loghetti inline-block">
+                        <Image width="50" height="50" alt="Tailwindcss" title="Tailwindcss" src={"/images/loghetto-tailwind.svg"} />
                         <p>TailwindCss</p>
                     </div>
-                    <div className="inline-flex">
-                        <Image width="50" height="50" alt="Bootstrap" title="Bootstrap" src={"@/images/"} />
+                    <div className="loghetti inline-block">
+                        <Image width="50" height="50" alt="Bootstrap" title="Bootstrap" src={"/images/loghetto-bootstrap.svg"} />
                         <p>Bootstrap</p>
                     </div>
                 </div>
