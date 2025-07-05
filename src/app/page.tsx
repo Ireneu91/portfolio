2a0a177d9592dc7d7e0cc19470847850aae6a07b
Ireneu91 <irene.ucciero91@gmail.com>
2025-07-05 18:46:30 +0200

    favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Cover } from "@/components/Cover";
 import {Projects} from "@/components/Projects"
 
-// import {Helmet} from "react-helmet";
 import Script from 'next/script'
 
 import '@/assets/fonts/montserrat/stylesheet.css'
@@ -10,6 +9,7 @@ import '@/assets/global.css'
 export default function Home() {
   return (
     <main className="container mx-auto">
+
       <Script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js" />
 			<Script src="js/jquery.fancybox.js" />
       <Cover />
