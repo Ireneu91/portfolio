796f09f88106336e2ea3026b2cda6bf7799235cd
Ireneu91 <irene.ucciero91@gmail.com>
2025-07-05 18:21:14 +0200

    fix env variable error

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  publicRuntimeConfig: {
-    basePath: process.env.NEXT_PUBLIC_NODE_ENV === 'production' ? "/portfolio" : '',
-  },
 };
 
 export default nextConfig;
--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,16 +1,11 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 import React from "react";
 
 export const generateImageLink = (img: string) => {
-    const isProduction = process.env.NEXT_PUBLIC_NODE_ENV !== 'development' || true;
-
-    if (isProduction) {
-        return `/portfolio/${img}`;
-    }
-
-    return img;
+    const basePath = process.env.NEXT_PUBLIC_NODE_ENV === 'development' ? "" : '/portfolio'
+    return `${basePath}${img}`;
 }
 
 export const Image = (props: any) => {
